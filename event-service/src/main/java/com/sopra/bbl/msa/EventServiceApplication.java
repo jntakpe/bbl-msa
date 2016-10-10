86112d4f8e86d34f6f3b8d7289b9450c107b6f78
jntakpe <j.ntakpe@gmail.com>
2016-10-10 17:49:37 +0200

    #5 Initialisation de l'application de gestion des événements

---
+++ event-service/src/main/java/com/sopra/bbl/msa/EventServiceApplication.java
@@ -0,0 +1,16 @@
+package com.sopra.bbl.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventServiceApplication.class, args);
+    }
+}
