33eea87a246b435607242092f9cdafda4863c67b
jntakpe <j.ntakpe@gmail.com>
2016-10-10 22:44:58 +0200

    #8 Initialisation du module de gestion des notifications

---
+++ notification-service/src/main/java/com/sopra/bbl/msa/notifications/NotificationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.sopra.bbl.msa.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
