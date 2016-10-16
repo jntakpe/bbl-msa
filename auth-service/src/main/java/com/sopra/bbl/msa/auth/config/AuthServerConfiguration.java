8e4f01a20f2fed160ae44086e62f53f6a6375317
jntakpe <j.ntakpe@gmail.com>
2016-10-16 20:57:57 +0200

    #14 Utilisation de JWT pour le token OAuth2

--- auth-service/src/main/java/com/sopra/bbl/msa/auth/config/AuthServerConfiguration.java
+++ auth-service/src/main/java/com/sopra/bbl/msa/auth/config/AuthServerConfiguration.java
@@ -1,12 +1,16 @@
 package com.sopra.bbl.msa.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * Configuration du serveur authorization
@@ -25,6 +29,18 @@ public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapte
         this.authenticationManager = authenticationManager;
     }
 
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("bblmsasigninkey");
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
@@ -37,7 +53,10 @@ public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapte
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(this.authenticationManager);
+        endpoints
+                .tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter())
+                .authenticationManager(this.authenticationManager);
     }
 
 }
