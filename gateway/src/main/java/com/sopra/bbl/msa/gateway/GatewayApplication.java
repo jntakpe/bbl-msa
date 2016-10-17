39ffe5898df7441370310a233c2034b1a082311d
jntakpe <j.ntakpe@gmail.com>
2016-10-17 10:28:46 +0200

    #14 Vérification des tokens au niveau de la Gateway

--- gateway/src/main/java/com/sopra/bbl/msa/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/sopra/bbl/msa/gateway/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.sopra.bbl.msa.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  *
  * @author jntakpe
  */
+@EnableOAuth2Sso
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
