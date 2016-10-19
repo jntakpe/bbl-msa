87fdc8c545cf79f8b0095976ba4c4b8086e3cb71
jntakpe <j.ntakpe@gmail.com>
2016-10-19 09:22:10 +0200

    #17 Ajout de données au module profile

--- auth-service/src/main/resources/import.sql
+++ auth-service/src/main/resources/import.sql
@@ -1,16 +1,16 @@
 --@formatter:off
 
--- Table Account
+-- Table account
 INSERT INTO account (login, password) VALUES ('jntakpe', 'bblmsapwd');
 INSERT INTO account (login, password) VALUES ('bpoindron', 'bblmsapwd');
 INSERT INTO account (login, password) VALUES ('cbarillet', 'bblmsapwd');
 INSERT INTO account (login, password) VALUES ('rjansem', 'bblmsapwd');
 
--- Table Authority
+-- Table authority
 INSERT INTO authority (name) VALUES ('USER');
 INSERT INTO authority (name) VALUES ('ADMIN');
 
--- Table de jointure Account_Authorities
+-- Table de jointure account_authorities
 INSERT INTO account_authorities (account_id, authorities_id) VALUES ((SELECT id FROM account WHERE login='jntakpe'), (SELECT id FROM authority WHERE name='USER'));
 INSERT INTO account_authorities (account_id, authorities_id) VALUES ((SELECT id FROM account WHERE login='bpoindron'), (SELECT id FROM authority WHERE name='USER'));
 INSERT INTO account_authorities (account_id, authorities_id) VALUES ((SELECT id FROM account WHERE login='cbarillet'), (SELECT id FROM authority WHERE name='USER'));
