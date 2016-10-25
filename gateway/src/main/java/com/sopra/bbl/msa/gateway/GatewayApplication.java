14cc42357b824503f1521847f2169cc306ae2f6c
jntakpe <j.ntakpe@gmail.com>
2016-10-25 09:50:36 +0200

    Utilisation de Zuul en Gateway et RP

---
+++ gateway/src/main/java/com/sopra/bbl/msa/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.sopra.bbl.msa.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Classe démarrant le portail Zuul point d'entrée unique des services
+ *
+ * @author jntakpe
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ gateway/src/test/java/com/sopra/bbl/msa/gateway/GatewayApplicationTests.java
@@ -0,0 +1,21 @@
+package com.sopra.bbl.msa.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Tests vérifiant le démarrage de l'application
+ *
+ * @author jntakpe
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class GatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
